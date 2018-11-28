5a4605be06f621b4cf4c379304dcd542a82d084e
Shammi Hans <shammihans1@gmail.com>
2018-11-28 14:13:28 +0530

    updated text & functionalities, buttons generation on Enter key

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -4,12 +4,11 @@
             <div class="col-md-5">
                 <div class="panel panel-primary">
                     <div class="panel-heading">
-                        <h6 class="panel-title">Binding, v-for, v-model, v-on:click, v-on:input, v-bind:class</h6>
+                        <h6 class="panel-title">Binding, v-for, v-model, Key Modifiers(@keyup.enter), Event Modifiers(v-on:click.stop), v-bind:class</h6>
                     </div>
                     <div class="panel-body">
                         <div class="form-group">
-                            <i class="fas m-rl-5 fa-2x add-tags fa-plus-circle text-success cur-p" v-on:click="setTags()"></i>
-                            <input type="text" class="form-control" placeholder="Type tags & press Enter" v-model="tagItem" v-on:input="setTags2($event)" >
+                            <input type="text" class="form-control" placeholder="Type tags & press Enter" v-model="tagItem" @keyup.enter="setTags()" >
                         </div>
                     </div>
                 </div>
@@ -21,7 +20,9 @@
             </div>
             <div class="col-md-7">
                 <div class="d-flex flex-wrap">
-                    <div class="p-2 btn" v-for="(item, index) in tagArray" :key='index' v-bind:class="{ 'bg-warning': (index%2 === 0), 'bg-info': (index%2 !== 0) }">{{index}}. {{item}}</div>
+                    <div class="p-2 btn" v-for="(item, index) in tagArray" :key='index' v-bind:class="{ 'bg-warning': (index%2 === 0), 'bg-info': (index%2 !== 0) }">{{index}}. {{item}}
+                        <span class="close-icon" style="display: inline-block;" v-on:click.stop="remove"> x</span>
+                    </div>
                 </div>
             </div>
         </div>
@@ -37,17 +38,14 @@ export default {
         };
     },
     methods: {
-        setTags2(event) {
-            if (event.keycode === 13) {
-                this.tagArray.push(this.tagItem);
-                this.tagItem = '';
-            }
-        },
-        setTags() {
+        setTags: function() {
             if (this.tagItem !== '') {
                 this.tagArray.push(this.tagItem);
                 this.tagItem = '';
             }
+        },
+        remove: function(event) {
+            event.target.parentElement.remove();
         },
     },
 };
@@ -60,8 +58,11 @@ export default {
     input {
         max-width: 300px;
     }
-    .add-tags {
-        position: absolute;
-        right: 100px;
+    .close-icon {
+        padding: 5px;
+        margin-left: 10px;
+        text-transform: uppercase;
+        font-size: 10px;
+        border-left: 1px solid #a5a5a5;
     }
 </style>
